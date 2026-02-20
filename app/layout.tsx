70c46302f2a9439c34e5c5e816e65ccc28875ac4
GloriousEvolution1141 <adcoaquiraq@gmail.com>
2026-02-20 18:50:59 -0500

    update login redirect

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,8 +9,9 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "NextField - Admin",
+  //Añadir descripcion a posterior
+  description: "",
 };
 
 const geistSans = Geist({
